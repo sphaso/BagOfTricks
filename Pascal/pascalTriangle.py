dc6cbd1062ac08385b640e688b79819ab1cbb369
sphaso <sphaso@gmail.com>
2014-05-25 14:28:07 +0200

    added examples

--- Pascal/pascalTriangle.py
+++ Pascal/pascalTriangle.py
@@ -1,3 +1,12 @@
+# Given the definition of a Pascal triange (i.e.)
+# 1
+# 1 1
+# 1 2 1
+# 1 3 3 1
+# ...
+# Return the number in the sequence standing in
+# column "c" and row "r"
+
 def pascal(col, row):
     def calculateLine(lastRow):
         if (len(lastRow) == row + 1):
