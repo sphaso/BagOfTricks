fbd0968166572826538d9a05be943b10a6359e20
sphaso <sphaso@gmail.com>
2015-01-07 16:14:43 +0100

    Iterator C#

---
+++ DataStructures/Iterator/Iterator.Main/Iterator.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+
+namespace Iterator.Main
+{
+    interface IIterate<T>
+    {
+        T Next();
+
+        bool Eof();
+
+        T Current();
+    }
+
+    public class Iterator<T> : IIterate<T>
+    {
+        private readonly T[] _array;
+        private int _index;
+
+        public Iterator(T[] array)
+        {
+            _array = array;
+        }
+
+
+        public T Next()
+        {
+            if (!Eof())
+            {
+                _index += 1;
+                return Current();    
+            }
+
+            throw new IndexOutOfRangeException();
+        }
+
+        public bool Eof()
+        {
+            return _index == _array.Length - 1;
+        }
+
+        public T Current()
+        {
+            return _array[_index];
+        }
+    }
+}
