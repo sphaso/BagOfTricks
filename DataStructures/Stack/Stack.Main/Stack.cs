846e9408f7316498597ee7e314ca02c57f5fad0b
sphaso <sphaso@gmail.com>
2015-01-09 13:01:21 +0100

    Stack C#

---
+++ DataStructures/Stack/Stack.Main/Stack.cs
@@ -0,0 +1,44 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stack.Main
+{
+    public class Stack<V> : IStack<V>
+    {
+        private List<V> _storage = new List<V>();
+
+        public void Push(V entry)
+        {
+            _storage.Add(entry);
+        }
+
+        public V Pop()
+        {
+            var temp = Peek();
+            _storage.Remove(temp);
+
+            return temp;
+        }
+
+        public int Size()
+        {
+            return _storage.Count;
+        }
+
+        public V Peek()
+        {
+            return _storage.Last();
+        }
+
+        public bool IsEmpty()
+        {
+            return _storage.Count == 0;
+        }
+
+        public void Clear()
+        {
+            _storage.Clear();
+        }
+    }
+}
---
+++ DataStructures/Stack/Stack.Tests/StackTests.cs
@@ -0,0 +1,77 @@
+﻿using NUnit.Framework;
+using Stack.Main;
+
+namespace Stack.Tests
+{
+    [TestFixture]
+    public class StackTests
+    {
+        [Test]
+        public void Push()
+        {
+            var stack = new Stack<int>();
+            stack.Push(3);
+
+            Assert.That(() => stack.Peek(), Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Pop()
+        {
+            var stack = new Stack<int>();
+            stack.Push(3);
+            stack.Push(4);
+
+            Assert.That(() => stack.Pop(), Is.EqualTo(4));
+            Assert.That(() => stack.Size(), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Size()
+        {
+            var stack = new Stack<int>();
+            stack.Push(6);
+            stack.Push(26);
+
+            Assert.That(() => stack.Size(), Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Peek()
+        {
+            var stack = new Stack<int>();
+            stack.Push(4);
+            stack.Push(5);
+
+            Assert.That(() => stack.Peek(), Is.EqualTo(5));
+            Assert.That(() => stack.Size(), Is.EqualTo(2));
+        }
+
+        [Test]
+        public void IsEmptyOnEmptySet()
+        {
+            var stack = new Stack<int>();
+
+            Assert.That(() => stack.IsEmpty(), Is.True);
+        }
+
+        [Test]
+        public void IsEmptyOnFullSet()
+        {
+            var stack = new Stack<int>();
+            stack.Push(4);
+
+            Assert.That(() => stack.IsEmpty(), Is.False);
+        }
+
+        [Test]
+        public void ClearOnFullSet()
+        {
+            var stack = new Stack<int>();
+            stack.Push(4);
+            stack.Clear();
+
+            Assert.That(() => stack.IsEmpty(), Is.True);
+        }
+    }
+}
